511174813c271bdfeba63620482798635950f172
Sopiriye Robinson <sopiriyerobinson@gmail.com>
2026-05-25 13:21:11 +0100

    feat(auth): login dto that reflects single endpoint for multiple response in relation to user role........other node package were installed for possible future integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+//i need to be aware that i have to put logger where necessary.
+
+// i think i would need to put loggers at every NestJS request-response cycle, especially in the guards and services where critical operations happen, such as authentication and role checks. This way, I can track the flow of requests and identify any issues or unauthorized access attempts effectively.
